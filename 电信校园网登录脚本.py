c33055e4ea63c9b86dcc9438c03ef52d02249b93
suxin0203 <208082474@qq.com>
2022-03-16 00:25:30 +0800

    3-15更新所有py，且新增校园网登陆以及努努书房爬虫

--- 电信校园网登录脚本.py
+++ 电信校园网登录脚本.py
@@ -6,52 +6,75 @@
 
 import requests
 from lxml import etree
+import random
+
+
+def run(count1):
+    count1 = count1
+    num = random.randint(1000, 9999)
+    # 学校手机号
+    # userId = "1916152" + str(num)
+    userId = 19161536696
+    # print(userId)
+    # 校园网密码# 默认123456
+    passwd = "124203"
+
+    success = "离线"
+    success1 = "修改密码"
+    login = "登 录"
+    # 浏览器标头
+    headers = {
+        "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/94.0.4606.61 Safari/537.36 Edg/94.0.992.31"
+    }
+    # 创建地址
+    url = 'http://172.17.0.2/webauth.do?wlanacname=ZAX_BRAS_JYCJ&wlanuserip=10.2.156.238&mac=44:00:4d:35:e3:d3'
+    #
+    data = {
+        "loginType": "", "auth_type": "0", "isBindMac1": "0", "pageid": "3", "templatetype": "1", "listbindmac": "0",
+        "recordmac": "0", "isRemind": "1", "loginTimes": "", "groupId": "", "distoken": "", "echostr": "", "url": "",
+        "isautoauth": "", "notice_pic_loop2": "/portal/uploads/pc/demo2/images/bj.png",
+        "notice_pic_loop1": "/portal/uploads/pc/demo2/images/logo.png",
+        "userId": userId,
+        "passwd": passwd,
+        "remInfo": "on"
+    }
+    # 请求
+    response = requests.post(url=url, headers=headers, data=data)
+    r = response.content.decode()
+    # 验证网站源码是否反回
+    # print(r)
+    html = etree.HTML(r)
+    check = ""
+    check_r = ""
+    title1 = ""
+    successTitle = ""
+    try:
+        # 判断是否登录成功
+        inputq = html.xpath("/html/body/form[1]/div[1]/div[1]/div[2]/div[2]/div[3]/input/@placeholder")[0]
+        # print("未登录成功", inputq)
+        count1 = 0
+    except:
+        # 用户手机号
+        successTitle = html.xpath("/html/body/form[1]/div[1]/div[1]/div[2]/div[2]/div[3]/text()")[0]
+        # '离线'
+        check = html.xpath("/html/body/form[1]/div[1]/div[1]/div[2]/div[2]/div[5]/div/span/text()")[0]
+        # 修改密码
+        loginn = html.xpath("/html/body/form[1]/div[1]/div[1]/div[2]/div[2]/div[6]/div/span/text()")[0]
+        print(successTitle)
+        print(check)
+        print(loginn)
+        count1 = 1
+    finally:
+        print("登录失败-false!") if loginn == login else print("登录成功-success!")
+
+
+count1 = 0
+run(count1)
 
 
 
-# 学校手机号
-userId = "19161526795"
 
-# 校园网密码# 默认123456
-passwd = "1234567"
-
-
-
-
-
-
-
-
-success = "离线"
-success1 = "修改密码"
-# 浏览器标头
-headers = {
-    "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/94.0.4606.61 Safari/537.36 Edg/94.0.992.31"
-}
-# 创建地址
-url = 'http://172.17.0.2/webauth.do?wlanacname=ZAX_BRAS_JYCJ&wlanuserip=10.2.91.26&mac=44:00:4d:35:e3:d3'
-#
-data = {
-    "loginType": "", "auth_type": "0", "isBindMac1": "0", "pageid": "3", "templatetype": "1", "listbindmac": "0", "recordmac": "0", "isRemind": "1", "loginTimes": "", "groupId": "", "distoken": "", "echostr": "", "url": "", "isautoauth": "", "notice_pic_loop2": "/portal/uploads/pc/demo2/images/bj.png", "notice_pic_loop1": "/portal/uploads/pc/demo2/images/logo.png",
-    "userId": userId,
-    "passwd": passwd,
-    "remInfo": "on"
-}
-# 请求
-response = requests.post(url=url, headers=headers, data=data)
-r = response.content.decode()
-# 验证网站源码是否反回
-# print(r)
-html = etree.HTML(r)
-# 判断是否登录成功
-# '离线'
-check = html.xpath("/html/body/form[1]/div[1]/div[1]/div[2]/div[2]/div[5]/div/span/text()")[0]
-# '修改密码'
-check_r = html.xpath("/html/body/form[1]/div[1]/div[1]/div[2]/div[2]/div[6]/div/span/text()")[0]
-# '提示'
-title1 = html.xpath("/html/body/form[1]/div[1]/div[1]/div[2]/div[2]/div[4]/p/text()")[0]
-# 用户手机号
-successTitle = html.xpath("/html/body/form[1]/div[1]/div[1]/div[2]/div[2]/div[3]/text()")[0]
-print(successTitle)
-
-print("登录成功-success!", title1) if check == success or check_r == success1 else print("登录失败-false!")
+# for i in range(100):
+#     run(count1)
+#     if count1 == 1:
+#         break
---
+++ 软件9班作业提交人数查询脚本.py
@@ -0,0 +1,56 @@
+# -*- coding: utf-8 -*-
+
+# @Time    : 2021/10/11 18:39
+# @Author  : Suxin
+# @File    : 作业提交人数查询.py
+
+import os
+import os.path
+# 查询的文件目录
+path = "C:/Users/suxin/Documents/Tencent Files/208082474/FileRecv/20级软件1班"
+
+# arr 为遍历文件夹下提交人员名单 arr_all为总人数
+arr = []
+arr_all = ["刘薇", "张志军", "罗达", "陈同", "马真钛", "宋燕杰", "张自富", "龚飞", "杨利", "陈珂馨", "陈旭林", "张聪", "牛洪", "姜琴", "梁浩", "易友才", "赵礼超", "冯浪", "王江贵", "文海东", "王何平", "李虹成", "龙乙文", "李林辉", "巨鑫", "李婷婷", "闫何", "李青松", "唐宏", "刘川东", "蒋军", "杨海", "陈毅", "王鹏", "吴邦俊", "陈洪印", "王智强", "相博源", "李准", "刘良梁", "李正阳", "邓均", "彭俊杰", "秦洪波", "冯鹏", "马勇", "林春帆", "何东", "任雁宁", "廖豪", "涂浩源", "吴金龙", "顾焕良", "巴丁龙周"]
+all = len(arr_all)
+count = 0
+
+# 格式化文件名
+for parent, dirnames, filenames in os.walk(path):
+    for filename in filenames:
+        a = ''.join(filename.split())
+        b = a.replace('一班', '1班')
+        a = b.replace('2020级', '20级')
+        arr.append(a)
+
+# print(arr)
+# 未提交人数 will
+will = 1
+# 提交判断 t = 1 提交 默认为0
+t = 0
+
+# 循环遍历提交人员的名单是否再 总名单里面
+for i in range(len(arr)):
+    # print(arr_all[i])
+    j = arr[i]
+    for x in range(len(arr_all)):
+        # print(arr[x])
+        z = arr_all[x]
+        if z in j:
+            # print("-", z)
+            count = count + 1
+            arr_all.remove(z)
+            break
+for y in range(len(arr_all)):
+    will = will +1
+    print("未提交：", arr_all[y])
+print("-"*50+"")
+print("总人数：", all)
+print("-"*50+"")
+print("总共提交人数：", count)
+print("-"*50+"")
+print("未提交人数：", will-1)
+
+
+
+
